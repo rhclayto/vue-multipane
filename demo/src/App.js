8152bf511e0b0c3d21de8e1fb50e8be905bd3db0
rhclayto <h@obscur.us>
2020-11-07 02:41:32 -0700

    Update App.js

--- demo/src/App.js
+++ demo/src/App.js
@@ -2,12 +2,16 @@ import VerticalPanes from './VerticalPanes.vue';
 import HorizontalPanes from './HorizontalPanes.vue';
 import CustomResizer from './CustomResizer.vue';
 import CombinedVerticalHorizontalPanes from './CombinedVerticalHorizontalPanes.vue';
+import FlexGrowOnTheLeft from './FlexGrowOnTheLeft.vue';
+import CombinedVerticalHorizontalPanes from './CombinedVerticalHorizontalPanes.vue';
 
 export default {
   components: {
     VerticalPanes,
     HorizontalPanes,
     CustomResizer,
+    CombinedVerticalHorizontalPanes,
+    FlexGrowOnTheLeft,
     CombinedVerticalHorizontalPanes,
   },
 };
